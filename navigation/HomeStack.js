819cdac028c40dbb52e2466bf2014249ee6f7dcd
Andy <andyhz822@gmail.com>
2022-05-04 19:39:41 -0700

    Choose your avatar screen

--- navigation/HomeStack.js
+++ navigation/HomeStack.js
@@ -3,6 +3,7 @@ import { createMaterialBottomTabNavigator } from "@react-navigation/material-bot
 import HomeScreen from "../screens/HomeScreen";
 import ExtraScreen from "../screens/ExtraScreen";
 import UserProfileScreen from "../screens/UserProfileScreen";
+import IconScreen from "../screens/IconScreen";
 import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
 import MaterialIcons from "react-native-vector-icons/MaterialIcons";
 
@@ -43,6 +44,15 @@ export default function HomeStack() {
           ),
         }}
       />
+        <Tab.Screen
+            name="Icon"
+            component={IconScreen}
+            options={{
+                tabBarIcon: ({ color }) => (
+                    <MaterialCommunityIcons name="account-box" color={color} size={26} />
+                ),
+            }}
+        />
     </Tab.Navigator>
   );
 }
--- styles.js
+++ styles.js
@@ -1,4 +1,6 @@
 import { StyleSheet } from "react-native";
+import { Dimensions } from "react-native";
+const { width, height } = Dimensions.get('screen');
 
 export default StyleSheet.create({
   container: {
@@ -18,4 +20,34 @@ export default StyleSheet.create({
     height: 250,
     width: 250,
   },
+
+  background:{
+    display: "flex",
+    flexDirection: "row",
+    flexWrap: "wrap",
+    alignContent: "center",
+    alignItems: "center",
+    justifyContent: "center"
+  },
+
+  avatar: {
+    flexDirection: "row",
+    flexWrap: "wrap",
+    height: "20%",
+    width: "25%",
+    padding: 50,
+    marginLeft: "2%",
+    marginRight: "2%",
+    marginBottom: "2%",
+    resizeMode: "contain",
+  },
+  headline_text: {
+    color: 'black',
+    fontSize: 30,
+    fontWeight: 'bold',
+    marginTop: "10%",
+    marginLeft: "10%",
+    marginRight: "10%",
+  },
+
 });
