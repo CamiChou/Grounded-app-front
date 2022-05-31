b4bd51f4a4aa9f1fae5a7e5b833010336f28f477
jason2020 <jtay20@punahou.edu>
2022-05-31 01:16:52 -0700

    Almost complete post page

--- screens/CameraScreen.js
+++ screens/CameraScreen.js
@@ -5,12 +5,16 @@ import {
   TouchableOpacity,
   ImageBackground,
   Image,
+  Switch,
 } from "react-native";
 import { Camera } from "expo-camera";
 import { AuthContext } from "../navigation/AuthProvider";
 import { uploadCloudStorage } from "../firebase/firebaseFunctions";
 import * as Location from "expo-location";
 import { Icon } from "react-native-elements";
+import { Picker } from "@react-native-picker/picker";
+import { TextInput } from "react-native-gesture-handler";
+import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
 
 export default function CameraScreen({ navigation }) {
   const { user, logout } = useContext(AuthContext);
@@ -20,6 +24,9 @@ export default function CameraScreen({ navigation }) {
   const [type, setType] = useState(Camera.Constants.Type.back);
   const [flash, setFlash] = useState(Camera.Constants.FlashMode.off);
   const [pin, setPin] = React.useState(null); // pri Create a pin here to show current location
+  const [publicPost, setPublic] = useState(false);
+  const [isEnabled, setIsEnabled] = useState(false);
+  const toggleSwitch = () => setIsEnabled((previousState) => !previousState);
 
   useEffect(() => {
     (async () => {
@@ -86,88 +93,196 @@ export default function CameraScreen({ navigation }) {
         }}
       >
         {previewVisible ? (
-          <View style={{
-            flex: 1,
-            flexDirection: "column",
-          }}>
-            <View style={{
-              flex: 2,
-              flexDirection: "row"
-            }}>
-              <ImageBackground
-                source={{ uri: capturedImage && capturedImage.uri }}
-                // resizeMode="fit"
+          <KeyboardAwareScrollView
+            resetScrollToCoords={{ x: 0, y: 0 }}
+            contentContainerStyle={{ flex: 1 }}
+            scrollEnabled={false}
+          >
+            <View
+              style={{
+                flex: 1,
+                flexDirection: "column",
+              }}
+            >
+              <View
                 style={{
-                  flex: 3,
+                  flex: 1,
+                  flexDirection: "row",
+                  width: "100%",
+                  justifyContent: "center",
+                  alignItems: "center",
                 }}
               >
-                {/* <TouchableOpacity
+                <Text
                   style={{
-                    color: "#fff",
+                    marginTop: 10,
                     fontSize: 20,
-                    width: 350,
-                    height: 350,
-                    borderColor: "#fff",
-                    borderWidth: 2,
-                    marginTop: 200,
-                    marginLeft: 20,
+                    alignSelf: "center",
+                    textAlign: "center",
+                  }}
+                >
+                  New Post
+                </Text>
+                <TouchableOpacity
+                  style={{
+                    position: "absolute",
+                    top: "40%",
+                    left: "5%",
+                  }}
+                >
+                  <Icon
+                    name="arrow-back"
+                    color="black"
+                    onPress={() => {
+                      setPreviewVisible(false);
+                    }}
+                  />
+                </TouchableOpacity>
+              </View>
+              <View
+                style={{
+                  flex: 2,
+                  flexDirection: "row",
+                }}
+              >
+                {/* TODO: Crop image */}
+                <ImageBackground
+                  source={{ uri: capturedImage && capturedImage.uri }}
+                  resizeMode="cover"
+                  style={{
+                    flex: 3,
                   }}
                 />
                 <View
                   style={{
-                    flex: 1,
+                    flex: 2,
+                    justifyContent: "center",
+                    alignItems: "center",
                     flexDirection: "column",
-                    padding: 15,
-                    justifyContent: "flex-end",
+                  }}
+                >
+                  <View style={{ flex: 1 }}>
+                    <Text style={{ fontSize: 12, marginTop: 10 }}>
+                      Who can see this post?
+                    </Text>
+                  </View>
+                  <View
+                    style={{
+                      flex: 1,
+                      justifyContent: "center",
+                      alignItems: "center",
+                    }}
+                  >
+                    <Picker
+                      selectedValue={publicPost}
+                      onValueChange={(itemValue) => setPublic(itemValue)}
+                      style={{ height: "100%", width: 150, marginBottom: 260 }}
+                    >
+                      <Picker.Item label="Only Me" value="false" />
+                      <Picker.Item label="Public" value="true" />
+                    </Picker>
+                  </View>
+                </View>
+              </View>
+              <View
+                style={{
+                  flex: 3,
+                  flexDirection: "column",
+                  justifyContent: "center",
+                  alignItems: "center",
+                }}
+              >
+                <View
+                  style={{
+                    flex: 1,
+                    width: "90%",
+                    borderBottomColor: "grey",
+                    borderBottomWidth: 1,
+                    justifyContent: "center",
+                    alignItems: "right",
+                    flexDirection: "row",
                   }}
                 >
                   <View
                     style={{
-                      flexDirection: "row",
-                      justifyContent: "space-between",
+                      flex: 1,
+                      height: "100%",
+                      justifyContent: "center",
+                      alignItems: "right",
+                    }}
+                  >
+                    <Switch
+                      style={{ transform: [{ scaleX: 0.7 }, { scaleY: 0.7 }] }}
+                      trackColor={{ false: "#767577", true: "#9CC991" }}
+                      thumbColor={isEnabled ? "#f4f3f4" : "#f4f3f4"}
+                      ios_backgroundColor="#3e3e3e"
+                      onValueChange={toggleSwitch}
+                      value={isEnabled}
+                    />
+                  </View>
+                  <View
+                    style={{
+                      flex: 5,
+                      height: "100%",
+                      justifyContent: "center",
+                      alignItems: "right",
+                    }}
+                  >
+                    <Text>Add Location</Text>
+                  </View>
+                </View>
+                <View
+                  style={{
+                    flex: 4,
+                    width: "100%",
+                    flexDirection: "column",
+                  }}
+                >
+                  <View
+                    style={{
+                      flex: 1,
+                      justifyContent: "center",
+                      alignItems: "center",
+                    }}
+                  >
+                    <TextInput
+                      style={{
+                        color: "black",
+                        width: "90%",
+                        height: "100%",
+                        textAlignVertical: "top",
+                        padding: 10,
+                      }}
+                      placeholder="Caption:"
+                      placeholderTextColor="black"
+                      numberOfLines={5}
+                      multiline
+                    ></TextInput>
+                  </View>
+                  <View
+                    style={{
+                      flex: 1,
+                      alignItems: "center",
                     }}
                   >
                     <TouchableOpacity
-                      onPress={() => setPreviewVisible(false)}
                       style={{
-                        width: 130,
-                        height: 40,
-                        alignItems: "center",
-                        borderRadius: 4,
+                        marginTop: 30,
+                        backgroundColor: "#9CC991",
+                        paddingTop: 10,
+                        paddingBottom: 10,
+                        paddingLeft: 20,
+                        paddingRight: 20,
+                        borderRadius: 20,
                       }}
                     >
-                      <Text
-                        style={{
-                          color: "#fff",
-                          fontSize: 20,
-                        }}
-                      >
-                        Retake
-                      </Text>
-                    </TouchableOpacity>
-                    <TouchableOpacity
-                      onPress={() => uploadImageAsync(capturedImage.uri)}
-                      style={{
-                        width: 130,
-                        height: 40,
-                        alignItems: "center",
-                        borderRadius: 4,
-                      }}
-                    >
-                      <Text
-                        style={{
-                          color: "#fff",
-                          fontSize: 20,
-                        }}
-                      >
-                        Save
-                      </Text>
+                      <Text>Share</Text>
                     </TouchableOpacity>
                   </View>
-                </View> */}
-              </ImageBackground>
+                </View>
+              </View>
             </View>
-          </View>
+          </KeyboardAwareScrollView>
         ) : (
           <Camera
             style={{ flex: 1 }}
@@ -190,7 +305,9 @@ export default function CameraScreen({ navigation }) {
                   top: "10%",
                   left: "4%",
                 }}
-                onPress={() => { navigation.navigate("Home"); }}
+                onPress={() => {
+                  navigation.navigate("Home");
+                }}
               >
                 <Icon name="close" color="white" />
               </TouchableOpacity>
@@ -257,11 +374,15 @@ export default function CameraScreen({ navigation }) {
                       backgroundColor: "#fff",
                     }}
                   >
-                    <Image style={{
-                      width: "100%",
-                      height: "100%",
-                      borderRadius: 50,
-                    }} source={require("../assets/logo.png")} resizeMode="contain" />
+                    <Image
+                      style={{
+                        width: "100%",
+                        height: "100%",
+                        borderRadius: 50,
+                      }}
+                      source={require("../assets/logo.png")}
+                      resizeMode="contain"
+                    />
                   </TouchableOpacity>
                 </View>
               </View>
