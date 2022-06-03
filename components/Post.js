55d3503b6bfe5a9ce8e6a3f28e7365c31fdfbdef
rachelnguyen <rachelnguyn@gmail.com>
2022-06-02 19:03:54 -0700

    Merge branch 'posts' into main

--- components/Post.js
+++ components/Post.js
@@ -8,108 +8,62 @@ import {
   Image,
 } from "react-native";
 import PostStyles from "../styles/PostStyles";
-import { Dimensions } from "react-native";
-import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
+import ReadMore from 'react-native-read-more-text';
+
 
 export default function Post({ data }) {
   return (
-    // <View style={PostStyles.post}>
-    //   <View style={PostStyles.postImage}>
-    //     <View style={PostStyles.postHeader}>
-    //       <View style={PostStyles.userProfile}></View>
-    //       <View style={PostStyles.userInfo}>
-    //         <View style={PostStyles.user}>
-    //           <Text style={PostStyles.userName}>{data.user}</Text>
-    //           <Text style={PostStyles.type}>{` ${data.type}`}</Text>
-    //         </View>
-    //         <Text style={PostStyles.userTime}>{data.time}</Text>
-    //       </View>
-    //       <View style={PostStyles.userLocation}></View>
-    //     </View>
-    //     <Image source={data.image} style={PostStyles.image} />
-    //   </View>
-    //   <View style={PostStyles.postMessage}>
-    //     <Text>{data.caption}</Text>
-    //   </View>
-    // </View>
-
-  // <View style={{    flex: 1,
-  //   width: 375,
-  //   height: 436,
-  //   marginBottom: 10,
-  //   flexDirection: "column",
-  //   borderRadius: 25,
-  //   backgroundColor: "#E7FFEB"}}>
-  //     <Image source={data.image} style={{
-  //       height:undefined,
-  //       width: '100%',
-  //       aspectRatio: 1,
-  //       borderTopLeftRadius: 25,
-  //       borderTopRightRadius: 25
-  //     }}></Image>
-  //     <View style={{
-  //       height: 52, width: 375, position: 'relative', bottom: 375,
-  //       backgroundColor: 'white', borderTopLeftRadius: 25,
-  //       borderTopRightRadius: 25, opacity: 0.8
-  //     }}></View>
-  //     <View style={{
-  //       height: 76, width: 375, position: 'relative'
-  //     }}>
-  //       <Text style={{
-  //         fontSize: 15, position: 'relative', bottom: 50, marginVertical: 7, marginHorizontal: 15
-  //         }}>{data.caption}</Text>
-  //     </View>
-  // </View>
-
-<View style={{    flex: 1,
-    width: Dimensions.get("window").width,
-    height: undefined,
-    marginBottom: 10,
-    flexDirection: "column",
-    backgroundColor: "white"}}>
-
-    <ImageBackground source={data.image} style={{
-      height:307,
-      width: Dimensions.get("window").width,
-    }}>
-
-    <View style={{
-        top: 15,
-        marginHorizontal: 15,
-        zIndex:1,
-        flexDirection: "row",
-        alignItems: "center", 
-        position: 'relative'
-      }}>
-        <Image style={{height: 40, width:40}} resizeMode="contain" source={require("../assets/avatars/avatar1.png")}></Image>
-        <Text style={{fontSize: 18, position: "relative", left: 13, color: "white", bottom: 7}}>{data.user}</Text>
-        <Text style={{fontSize: 12, position: "relative", left: 20, color: "#DADADA", bottom: 4.3, fontStyle:"italic"}}>{data.type}</Text>
-        <Text style={{fontSize: 14, position: "absolute", left: 53 , color: "#DADADA", top:27, fontStyle:"italic"}}>{data.time}</Text>
-
-      { data.location ? <View style={{flexDirection: "row",
-            alignItems: "center", right: 0, position: 'absolute'}}>
-        <View style={{
-            position: "relative",bottom: 3,
-          }}>
-          <MaterialCommunityIcons
-            name="map-marker-outline"
-            color="#2664DC"
-            size={28}
-            />
+    <View style={PostStyles.post}>
+      
+      <View style={PostStyles.postImage}>
+      <ImageBackground source={data.image} style={PostStyles.image}>
+        <View style={PostStyles.postHeader}>
+          <View style={PostStyles.userProfile}><Image source={data.pfp} style={PostStyles.profImg}/></View>
+          <View style={PostStyles.userInfo}>
+            <View style={PostStyles.user}>
+              <Text style={PostStyles.userName}>{data.user}</Text>
+              <Text style={PostStyles.type}>{` ${data.type}`}</Text>
             </View>
-          <Text style={{fontSize: 15, position: "relative", color: "white", bottom: 4, fontStyle:"italic"}}>{JSON.stringify(data.location, ['longitude'])}</Text>
-
-        </View> : null }
+            <Text style={PostStyles.userTime}>{data.time}</Text>
+          </View>
+          <View style={PostStyles.userLocation}></View>
         </View>
-    </ImageBackground>
-
-    <View style={{
-      height: 70, width: 375, 
-    }}>
-      <Text style={{
-        fontSize: 15, position: 'relative', marginVertical: 15, marginHorizontal: 20
-        }}>{data.caption}</Text>
+        </ImageBackground>
+      
+      </View>
+      <View style ={PostStyles.messageContainer}>
+      <View style={PostStyles.postMessage}>
+      <ReadMore
+              numberOfLines={2}
+              renderTruncatedFooter={this._renderTruncatedFooter}
+              renderRevealedFooter={this._renderRevealedFooter}
+              onReady={this._handleTextReady}>
+        <Text>{data.caption}</Text>
+      </ReadMore>
+      </View>
+      </View>
+    
     </View>
-</View>
   );
+
+
+_renderTruncatedFooter = (handlePress) => {
+  return (
+    <Text style={{color: Colors.tintColor, marginTop: 5}} onPress={handlePress}>
+      Read more
+    </Text>
+  );
+}
+
+_renderRevealedFooter = (handlePress) => {
+  return (
+    <Text style={{color: Colors.tintColor, marginTop: 5}} onPress={handlePress}>
+      Show less
+    </Text>
+  );
+}
+
+_handleTextReady = () => {
+  // ...
+}
 }
--- styles/PostStyles.js
+++ styles/PostStyles.js
@@ -5,31 +5,25 @@ export default StyleSheet.create({
   post: {
     flex: 1,
     width: Dimensions.get("window").width,
-    height: 300,
-    marginBottom: 100,
     flexDirection: "column",
-    borderRadius: 80,
-    backgroundColor: "black"
   },
   postHeader: {
     justifyContent: "space-between",
     flexDirection: "row",
-    backgroundColor: "white",
     flex: 1,
     width: "100%",
-    height: "40%",
+    height: "25%",
     zIndex: 1,
+    marginTop: '2.5%'
   },
   userProfile: {
     flex: 1,
     width: "20%",
     height: "100%",
-    backgroundColor: "white",
   },
   userInfo: {
     flex: 4,
     height: "100%",
-    backgroundColor: "white",
   },
   user: {
     flex: 1,
@@ -38,38 +32,51 @@ export default StyleSheet.create({
   userLocation: {
     flex: 1,
     height: "100%",
-    backgroundColor: "white",
   },
   type: {
     fontSize: 12,
-    color: "black",
+    color: "#DADADA",
+    marginLeft: '1%',
+    fontStyle: "italic",
   },
   userName: {
     fontSize: 14,
-    color: "black",
+    color: "white",
   },
   userTime: {
     fontSize: 12,
-    color: "black",
+    height: "95%",
+    color: "#DADADA",
     fontStyle: "italic",
   },
   postImage: {
     width: "100%",
+    height:309,
     flex: 1,
     alignItems: "center",
-    height: "30%",
   },
   image: {
     resizeMode: "contain",
     width: "100%",
-    height: undefined,
-    aspectRatio: 1,
+    height: 309,
+  },
+  profImg:{
+    width: 40,
+    height: 40,
+    borderRadius: 40/ 2,
+    marginLeft: '10%',
+  },
+  messageContainer: {
+    backgroundColor: "white",
   },
   postMessage: {
     flexDirection: "row",
     justifyContent: "space-between",
-    paddingTop: 20,
-    borderTopWidth: 1,
+    paddingTop: 10,
+    marginLeft:'2.5%',
+    marginRight: '2.5%',
+    paddingBottom:10,
+    alignItems: "center",
     backgroundColor: "white",
   },
 });
